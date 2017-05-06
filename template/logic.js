b2d78be9a4d03412f8547216d1f09877614c2adb
Mujib Ansari <ansarimujiburrehman@gmail.com>
2017-05-06 17:28:00 +0530

    template updated

--- template/logic.js
+++ template/logic.js
@@ -2,10 +2,34 @@
 * @Author: Mujib Ansari
 * @Date:   2017-05-06 17:01:53
 * @Last Modified by:   Mujib Ansari
-* @Last Modified time: 2017-05-06 17:02:58
+* @Last Modified time: 2017-05-06 17:27:29
 */
 
 'use strict';
+var oScope = {};
+
 $( document ).ready( function() {
 	console.log( '%c Phaser Started. ', 'background: black; color: white' );
+	createGame.call( oScope );
 } );
+
+/*
+* Create the Game canvas
+*/
+function createGame() {
+	this.game = new Phaser.Game( 800, 600, Phaser.CANVAS, 'phaser-example', {
+		preload: preload,
+		create: create
+	} );
+};
+
+/*
+* Preload the assests
+*/
+function preload() {
+	console.log( '%c Assets preloaded. ', 'background: black; color: white' );
+};
+
+function create() {
+	console.log( '%c Component Creation Completed. ', 'background: black; color: white' );	
+};
